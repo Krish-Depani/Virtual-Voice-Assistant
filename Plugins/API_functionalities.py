7b0b6e197a10159801944db75815c54eeb989909
Krish-Depani <krishdepani@gmail.com>
2022-12-16 22:01:42 +0530

    changed api for IP address.

--- Plugins/API_functionalities.py
+++ Plugins/API_functionalities.py
@@ -3,7 +3,6 @@ import datetime
 from dotenv import load_dotenv
 from newsapi import NewsApiClient
 import re
-import faulthandler
 import requests
 from wolframalpha import Client
 
@@ -12,13 +11,12 @@ load_dotenv(dotenv_path='..\\Data\\.env')
 NEWS = os.getenv('NEWS_API')
 WOLFRAMALPHA = os.getenv('WOLFRAMALPHA_API')
 OPENWEATHERMAP = os.getenv('OPENWEATHERMAP_API')
-IPSTACK = os.getenv('IPSTACK_API')
 TMDB = os.getenv('TMDB_API')
 news = NewsApiClient(api_key=NEWS)
 
 def get_ip(_return=False):
     try:
-        response = requests.get(f'http://api.ipstack.com/check?access_key={IPSTACK}').json()
+        response = requests.get(f'http://ip-api.com/json/').json()
         if _return:
             return response
         else:
